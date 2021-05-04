f348cefaed3d3629b4dd673e30131e54c0c73a2b
Sai Lakshman <a.sailakshman@gmail.com>
2021-05-04 22:42:48 +0530

    Finshing Game logic, Dimensions Api

--- screens/Game.js
+++ screens/Game.js
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, Alert } from 'react-native'
+import { View, Text, Button, StyleSheet, Alert, ScrollView, FlatList } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
 import Card from '../components/Card';
 import NumberContainer from '../components/NumberContainer';
 import MainButton from '../components/MainButton';
+import BodyText from '../components/BodyText';
 import Colors from '../constants/colors'
 
 const generateRandomBetween = (min, max, exclude) => {
@@ -20,15 +21,16 @@ const generateRandomBetween = (min, max, exclude) => {
 }
 
 const Game = props => {
-    const [currentGuess, setcurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice))
-    const [rounds, setRounds] = useState(0)
+    const initialGuess = generateRandomBetween(1, 100, props.userChoice)
+    const [currentGuess, setcurrentGuess] = useState(initialGuess)
+    const [pastGuesses, setpastGuesses] = useState([initialGuess.toString()])
     const currentLow = useRef(1)
     const currentHigh = useRef(100)
     const { userChoice, onGameOver } = props;
 
     useEffect(() => {
         if (currentGuess === userChoice) {
-            onGameOver(rounds)
+            onGameOver(pastGuesses.length)
         }
     }, [currentGuess, userChoice, onGameOver])
 
@@ -42,13 +44,24 @@ const Game = props => {
             currentHigh.current = currentGuess
         }
         else {
-            currentLow.current = currentGuess
+            currentLow.current = currentGuess + 1
         }
 
         const nextGuess = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess)
         setcurrentGuess(nextGuess)
-        setRounds(currentRounds => currentRounds + 1);
+        // setRounds(currentRounds => currentRounds + 1);
+
+        setpastGuesses(currPastGuess => [nextGuess.toString(), ...currPastGuess])
+
     }
+
+    const renderListItem = (listLength, itemData) => {
+        return (<View style={styles.listItem}>
+            <BodyText>#{listLength - itemData.index}</BodyText>
+            <BodyText>{itemData.item}</BodyText>
+        </View>)
+    }
+
     return (
         <View style={styles.screen}>
             <Text>Opponent's Guess</Text>
@@ -59,11 +72,21 @@ const Game = props => {
                     <Ionicons name="md-remove" size={24} color="white" />
                 </MainButton>
                 <MainButton onPress={() => nextGuessHandler('greater')}>
-                <Ionicons name="md-add" size={24} color="white" />
+                    <Ionicons name="md-add" size={24} color="white" />
                 </MainButton>
                 {/* <Button title="LOWER" onPress={()=> nextGuessHandler('lower')}/>
                 <Button title="GREATER" onPress={()=> nextGuessHandler('greater')}/> */}
             </Card>
+            <View style={styles.listContainer}>
+                {/* <ScrollView contentContainerStyle={styles.list}>
+                {pastGuesses.map((guess,index) => renderListItem(guess, pastGuesses.length -index))}
+            </ScrollView> */}
+                <FlatList
+                    keyExtractor={(item) => item}
+                    data={pastGuesses}
+                    renderItem={renderListItem.bind(this, pastGuesses.length)}
+                    contentContainerStyle={styles.list} />
+            </View>
         </View>
     )
 }
@@ -86,6 +109,25 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 350,
         maxWidth: '80%'
+    },
+    listItem: {
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
+    listContainer: {
+        width: '60%',
+        flex: 1,
+        marginVertical: 10,
+    },
+    list: {
+        flexGrow: 1,
+        justifyContent: 'flex-end',
     }
 })
 
